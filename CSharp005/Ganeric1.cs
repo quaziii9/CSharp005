f623a2fc2c1ac9e0081aacef465eb034d179168c
KGA <KGA@DESKTOP-UM053PK>
2024-03-08 17:42:39 +0900

    초기 커밋.

---
+++ CSharp005/Ganeric1.cs
@@ -0,0 +1,102 @@
+﻿
+namespace CSharp005
+{
+    internal class Ganeric1
+    {
+
+        // 형식 매개변수 제약하기
+        // 일반화 자료형을 선언할때 제약조건은 걸어줌으로써 사용당시 쓸 수 있는 자료형을 제한
+
+        #region 제약조건
+        /* 타입 안정성 보장
+         where T : struct { } // struct로 제한(값 형식)
+         where T : class { } // 클래스로 제한(참조 형식)
+         where T : new() { } // 매개변수가 없는 생성자가 있는 자료형
+         where T : 기반클래스 { } // 파생클래스로 제한
+         where T : 인터페이스 { } // 인터페이스를 포함한 자료형만...
+        */
+        #endregion
+
+        // 다음과 같은 형식이 있다고 가정한다면
+        // 해당 일반화 메서드는 매개변수 T에 값 형식이어야 함(구조체는 값  형식)
+
+        #region Where T: struct
+        public static void ArrayCopy<T>(T[] source, T[] output) where T : struct 
+        {
+            for (int i = 0; i < source.Length; i++)
+            {
+                output[i] = source[i];
+            }
+        }
+        #endregion
+
+        #region Where T : class
+
+        class MyClass<T> where T : class
+        {
+            private T data;
+
+            public MyClass(T data) 
+            {
+                this.data = data;
+            }
+            public void Show()
+            {
+                if(data != null) Console.WriteLine(data);
+                else
+                {
+                    Console.WriteLine("ddd");
+                }
+            }
+        }
+        #endregion
+
+        #region Where T : 인터페이스
+
+        //class Interface<T> where T: IComparable<T>
+        //{
+
+        //}
+
+        public static T Bigger<T>(T left, T right) where T : IComparable<T> 
+        { 
+            if(left.CompareTo(right) > 0) 
+            {
+                return left;
+            }
+            else
+            {
+                return right;
+            }
+        }
+
+        #endregion
+
+        static void Main(string[] args)
+        {
+            ///////////////////////////////////////////////////////////////////////////////////
+            int[] iSrc = { 1, 2, 3 }; 
+            int[] iDst = new int[iSrc.Length];
+            ArrayCopy<int>(iSrc, iDst);
+
+            string[] sSrc = { "파묘", "카리나", "헤어져" };
+            string[] sDst = new string[sSrc.Length];
+            // ArracyCopy<string>(sSrc, sDst); -> 에러 클래스는 참조형식이기 때문
+
+            ///////////////////////////////////////////////////////////////////////////////////
+
+            MyClass<string> str = new MyClass<string>("오태식이... ");
+            str.Show();
+            // MyClass<int> my = new MyClass<int>(1); // -> 값 형식이기 때문에 에러
+
+
+
+            ///////////////////////////////////////////////////////////////////////////////////
+            // Interface<int> interT = new Interface<int>();
+
+            int test = Bigger(10, 5);
+            Console.Write(test);
+
+        }
+    }
+}
---
+++ CSharp005/Program.cs
@@ -0,0 +1,165 @@
+﻿namespace CSharp005
+{
+
+    // Generic 
+    
+
+    internal class Program
+    {
+
+        #region 일반화 메서드
+
+        public static void SwapValue<T>(ref T a, ref T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
+        }
+        public static void ArrayCopy<T>(T[] source, T[] output)
+        {
+            for (int i = 0; i < source.Length; i++)
+            {
+                output[i] = source[i];
+            }
+        }
+
+        #endregion
+
+        #region 일반화 클래스
+        // 클래스에 필요한 자료형을 일반화한다.
+        // 클래스 인스턴스를 생성할때 자료형을 지정해서 사용
+
+        class SafeArray<T>
+        {
+            private T[] array;
+
+            // 배열의 크기를 인자로 받아서 초기화
+            public SafeArray(int size)
+            {
+                array = new T[size];
+            }
+
+            public void Set(int index, T value)
+            {
+                if (index < 0 || index >= array.Length) return;
+
+                array[index] = value;
+            }
+            public T Get(int index)
+            {
+                if (index < 0 || index>= array.Length)
+                {
+                    return default(T); // default : 자료형의 기본값 (T자료형의 기본값 0 리턴, 참조형은 NULL 리턴)
+                }
+                return array[index];
+            }
+        }
+        #endregion
+
+        class Item
+        {
+            private string Name;
+
+            public Item(string name) 
+            {
+                Name = name;
+            }
+            public string GetName()
+            {
+                return Name;
+            }
+        }
+
+        class Player
+        {
+            private SafeArray<Item> equippedItem;
+
+            public Player(int size)
+            {
+                equippedItem = new SafeArray<Item>(size);
+            }
+            public void EquipItem(int num, Item item)
+            {
+                equippedItem.Set(num, item);
+                Console.WriteLine($"장착된 {item.GetName()} 인덱스(칸){num}");
+            }
+            public Item GetEquippedItem(int num)
+            {
+                return equippedItem.Get(num);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            #region 메서드
+            //int x = 10;
+            //int y = 20;
+            ////SwapValue<int>(ref x, ref y);
+            //SwapValue(ref x, ref y);
+            //Console.WriteLine($"{x},{y}");
+            ////매개변수를 통해서 추측이 가능하면 생략가능
+
+            //double a = 1.1213;
+            //double b = 2.6546;
+
+            //SwapValue(ref a, ref b);
+            //Console.WriteLine($"{a},{b}");
+
+            //int[] iSrc = { 1, 2, 3, 4, 5, };
+            //int[] iDst = new int[iSrc.Length];
+            //ArrayCopy<int>(iSrc, iDst);
+            //foreach (int i in iDst) 
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //string[] sSrc = { "야호", "즐거운 금요일" };
+            //string[] sDst = new string[sSrc.Length];
+            //ArrayCopy(sSrc, sDst);
+            //foreach (var item in sDst)
+            //{
+            //    Console.Write(item);
+            //}
+            //Console.WriteLine();
+            #endregion
+
+            #region 클래스
+            //SafeArray<int> iArr = new SafeArray<int>(5);
+            //iArr.Set(0, 10);
+            //iArr.Set(6, 30);
+
+            //Console.WriteLine("인덱스 0 : {0}" , iArr.Get(0));
+            //Console.WriteLine("인덱스 6 : {0}", iArr.Get(6));  // 벗어나면 에러
+
+            ///////////////////////////////////////////////////////////////////////////////
+
+            Player player = new Player(5);
+
+            Item sword = new Item("칼");
+            Item shield = new Item("방패");
+            Item potion = new Item("빨간포션");
+
+            player.EquipItem(0, sword);
+            player.EquipItem(2, shield);
+            player.EquipItem(3, potion);
+
+            for(int i =0; i < 5;i++)
+            {
+
+                Item equipItem = player.GetEquippedItem(i);
+
+                if(equipItem != null)
+                {
+                    Console.WriteLine($"{i}칸 : {equipItem.GetName()}");
+                }
+                else
+                {
+                    Console.WriteLine($"{i}칸 : 없음" +
+                        $"");
+                }
+            }
+            #endregion
+
+
+        }
+    }
+}
